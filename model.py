90f984eb79237cf51aafddcfc00c152c2f7754e4
Abhinav Chanda <abhinav.chanda99@gmail.com>
2018-05-26 16:13:55 -0400

    Added Simple Notes

--- model.py
+++ model.py
@@ -1,21 +1,222 @@
+from __future__ import division
+
+#! /usr/bin/env python
+import math
+from math import log2, pow
 import sys
-import matplotlib.pyplot as plot
-from scipy.io import wavfile
+from aubio import source, pitch, tempo
 import peakdetect
+from numpy import median, diff
+import matplotlib.pyplot as plt
 
-def flatten(input):
-    new_list = []
-    for i in input:
-        for j in i:
-            new_list.append(j)
-    return new_list
 
-samplingFrequency, signalData = wavfile.read('twinkle.wav')
-plot.title('Spectrogram')
-spectrum, freqs, t, im = plot.specgram(signalData[:, 0],Fs=samplingFrequency)
-print(freqs)
-#_max, _min = peakdetect.peakdetect(spectrum, lookahead=300, delta=0.3)
+def get_file_bpm(path, params=None):
+    """ Calculate the beats per minute (bpm) of a given file.
+        path: path to the file
+        param: dictionary of parameters
+    """
+    if params is None:
+        params = {}
+    # default:
+    samplerate, win_s, hop_s = 44100, 1024, 512
+    if 'mode' in params:
+        if params.mode in ['super-fast']:
+            # super fast
+            samplerate, win_s, hop_s = 4000, 128, 64
+        elif params.mode in ['fast']:
+            # fast
+            samplerate, win_s, hop_s = 8000, 512, 128
+        elif params.mode in ['default']:
+            pass
+        else:
+            print("unknown mode {:s}".format(params.mode))
+    # manual settings
+    if 'samplerate' in params:
+        samplerate = params.samplerate
+    if 'win_s' in params:
+        win_s = params.win_s
+    if 'hop_s' in params:
+        hop_s = params.hop_s
+
+    s = source(path, samplerate, hop_s)
+    samplerate = s.samplerate
+    o = tempo("specdiff", win_s, hop_s, samplerate)
+    # List of beats, in samples
+    beats = []
+    # Total number of frames read
+    total_frames = 0
+
+    while True:
+        samples, read = s()
+        is_beat = o(samples)
+        if is_beat:
+            this_beat = o.get_last_s()
+            beats.append(this_beat)
+            #if o.get_confidence() > .2 and len(beats) > 2.:
+            #    break
+        total_frames += read
+        if read < hop_s:
+            break
+    '''
+    def beats_to_bpm(beats, path):
+        # if enough beats are found, convert to periods then to bpm
+        if len(beats) > 1:
+            if len(beats) < 4:
+                print("few beats found in {:s}".format(path))
+            bpms = 60./diff(beats)
+            return median(bpms)
+        else:
+            print("not enough beats found in {:s}".format(path))
+            return 0
+
+    return beats_to_bpm(beats, path)
+    '''
+    return len(beats)
+
+
+#if len(sys.argv) < 2:
+#    print("Usage: %s <filename> [samplerate]" % sys.argv[0])
+#    sys.exit(1)
+
+filename = 'Canon.wav'
+bpm = get_file_bpm(filename)
+print(bpm)
+
+downsample = 1
+samplerate = 44100 // downsample
+if len( sys.argv ) > 2: samplerate = int(sys.argv[2])
+
+win_s = 4096 // downsample # fft size
+hop_s = 512  // downsample # hop size
+
+s = source(filename, samplerate, hop_s)
+samplerate = s.samplerate
+
+tolerance = 0.8
+
+pitch_o = pitch("yin", win_s, hop_s, samplerate)
+pitch_o.set_unit("Hz")
+pitch_o.set_tolerance(tolerance)
+
+pitches = []
+confidences = []
+
+# total number of frames read
+total_frames = 0
+pitches2 = []
+times = []
+while True:
+    samples, read = s()
+    pitch = pitch_o(samples)[0]
+    #pitch = int(round(pitch))
+    confidence = pitch_o.get_confidence()
+    #if confidence < 0.8: pitch = 0.
+    pitches2.append(pitch)
+    times.append(total_frames / float(samplerate))
+    #print("%f %f %f" % (total_frames / float(samplerate), pitch, confidence))
+    pitches += [pitch]
+    confidences += [confidence]
+    total_frames += read
+    if read < hop_s: break
+
+l = len(times)/bpm
+arr = []
+count = 0
+max = 0
+
+for i in pitches2: 
+    if count < l:
+        count += 1 
+        if i > max:
+            max = i
+        
+    else:
+        if max >= 400 and max <= 1100 and (len(arr) == 0 or abs(max - arr[-1]) <= 400):
+            arr.append(max)
+        max = 0
+        count = 0   
+
+C0 = 440.0*math.pow(2, -4.75)
+name = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    
+def pitch(freq):
+    h = round(12*log2(freq/C0))
+    octave = h // 12
+    n = h % 12
+    return name[n] + str(octave)
+
+notes = []
+
+for i in arr:
+    notes.append(pitch(i))
+
+print(notes)   
+
+#print(pitches2)
+#_max, _min = peakdetect.peakdetect(pitches2, times, lookahead=200, delta=0)
 #print(_max)
-plot.xlabel('Time')
-plot.ylabel('Frequency')
-#plot.show()
+##plt.plot(times, pitches2)
+##plt.show()
+
+
+#_max, _min = peakdetect.peakdetect(pitches2, times, lookahead=300, delta=0)
+if 0: sys.exit(0)
+'''
+#print pitches
+import os.path
+from numpy import array, ma
+import matplotlib.pyplot as plt
+from demo_waveform_plot import get_waveform_plot, set_xlabels_sample2time
+
+skip = 1
+
+pitches = array(pitches[skip:])
+confidences = array(confidences[skip:])
+times = [t * hop_s for t in range(len(pitches))]
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(311)
+ax1 = get_waveform_plot(filename, samplerate = samplerate, block_size = hop_s, ax = ax1)
+plt.setp(ax1.get_xticklabels(), visible = False)
+ax1.set_xlabel('')
+
+def array_from_text_file(filename, dtype = 'float'):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    return array([line.split() for line in open(filename).readlines()],
+        dtype = dtype)
+
+ax2 = fig.add_subplot(312, sharex = ax1)
+ground_truth = os.path.splitext(filename)[0] + '.f0.Corrected'
+if os.path.isfile(ground_truth):
+    ground_truth = array_from_text_file(ground_truth)
+    true_freqs = ground_truth[:,2]
+    true_freqs = ma.masked_where(true_freqs < 2, true_freqs)
+    true_times = float(samplerate) * ground_truth[:,0]
+    ax2.plot(true_times, true_freqs, 'r')
+    ax2.axis( ymin = 0.9 * true_freqs.min(), ymax = 1.1 * true_freqs.max() )
+# plot raw pitches
+ax2.plot(times, pitches, '.g')
+# plot cleaned up pitches
+cleaned_pitches = pitches
+#cleaned_pitches = ma.masked_where(cleaned_pitches < 0, cleaned_pitches)
+#cleaned_pitches = ma.masked_where(cleaned_pitches > 120, cleaned_pitches)
+cleaned_pitches = ma.masked_where(confidences < tolerance, cleaned_pitches)
+ax2.plot(times, cleaned_pitches, '.-')
+#ax2.axis( ymin = 0.9 * cleaned_pitches.min(), ymax = 1.1 * cleaned_pitches.max() )
+#ax2.axis( ymin = 55, ymax = 70 )
+plt.setp(ax2.get_xticklabels(), visible = False)
+ax2.set_ylabel('f0 (midi)')
+
+# plot confidence
+ax3 = fig.add_subplot(313, sharex = ax1)
+# plot the confidence
+ax3.plot(times, confidences)
+# draw a line at tolerance
+ax3.plot(times, [tolerance]*len(confidences))
+ax3.axis( xmin = times[0], xmax = times[-1])
+ax3.set_ylabel('condidence')
+set_xlabels_sample2time(ax3, times[-1], samplerate)
+plt.show()
+#plt.savefig(os.path.basename(filename) + '.svg')
+'''
